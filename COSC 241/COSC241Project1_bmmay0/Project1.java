5ca0ca208ddb3a82cff030828287a8a646178857
FSUBryanMay <bmmay0@frostburg.edu>
2018-04-16 14:37:49 -0400

    Add comments and methodize code

--- COSC 241/COSC241Project1_bmmay0/Project1.java
+++ COSC 241/COSC241Project1_bmmay0/Project1.java
@@ -6,10 +6,15 @@ import java.util.Scanner;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+/**
+ * This program searches a dicitionary file for all the anagrams of a user inputted word
+ * 
+ * @author Bryan May
+ * @version 2018.04.16
+ */
 public class Project1{
     public static void main(String[] args) throws FileNotFoundException{
-        System.out.println("Welcome to Bryan May's anagram solver.");
-        System.out.println("Please enter the dictionary file you want to use:");
+        prompt();
 
         Scanner console = new Scanner(System.in);
         String dictionaryFile = console.nextLine();
@@ -18,6 +23,48 @@ public class Project1{
 
         Map<String, String> wordToSortedWord = new TreeMap<>();
 
+        convertDictionaryToCanonical(dictionaryScanner, wordToSortedWord);
+
+        System.out.println("Word to scramble (Enter N or n to quit):");
+
+        String wordOrQuit = console.next();
+
+        // As long as the exit code isn't keyed in,
+        // The program will continue to accept words
+        // And print the anagrams
+        while(!wordOrQuit.equals("N") && !wordOrQuit.equals("n")){
+
+            if(wordToSortedWord.keySet().contains(wordOrQuit)){
+                printAnagrams(wordOrQuit, wordToSortedWord);
+            }
+            else{
+                System.out.println("The word does not exist in the dictionary");
+            }
+
+            System.out.println("Word to scramble (Enter N or n to quit):");
+            
+            wordOrQuit = console.next();
+        }
+
+        console.close();
+        dictionaryScanner.close();
+    }
+
+    /**
+     * Introduction prompt of program
+     */
+    public static void prompt(){
+        System.out.println("Welcome to Bryan May's anagram solver.");
+        System.out.println("Please enter the dictionary file you want to use:");
+    }
+
+    /**
+     * Constructs a map mapping dictionary entries to their canonical forms
+     * 
+     * @param dictionaryScanner Scanner that reads the dictionary file
+     * @param wordToSortedWord Map that contains dictionary entries to canonical forms pairings
+     */
+    public static void convertDictionaryToCanonical(Scanner dictionaryScanner, Map<String, String> wordToSortedWord){
         while(dictionaryScanner.hasNext()){
             String word = dictionaryScanner.next();
 
@@ -37,27 +84,20 @@ public class Project1{
 
             wordToSortedWord.put(word, sortedWord);
         }
+    }
 
-        System.out.println("Word to scramble (Enter N or n to quit):");
+    /**
+     * Prints n >= 0 anagrams
+     * 
+     * @param wordToSortedWord Map that contains dictionary entries to canonical forms pairings
+     */
+    public static void printAnagrams(String wordOrQuit, Map<String, String> wordToSortedWord){
+        System.out.println("All words found in \"" + wordOrQuit + "\":");
 
-        String wordOrQuit = console.next();
-        while(!wordOrQuit.equals("N") && !wordOrQuit.equals("n")){
-            if(wordToSortedWord.keySet().contains(wordOrQuit)){
-                System.out.println("All words found in \"" + wordOrQuit + "\":");
-
-                for(String key: wordToSortedWord.keySet()){
-                    if(wordToSortedWord.get(key).equals(wordToSortedWord.get(wordOrQuit)) && key.length() == wordOrQuit.length()){
-                        System.out.println(key);
-                    }
-                }
+        for(String key: wordToSortedWord.keySet()){
+            if(wordToSortedWord.get(key).equals(wordToSortedWord.get(wordOrQuit)) && key.length() == wordOrQuit.length()){
+                System.out.println(key);
             }
-            else{
-                System.out.println("The word does not exist in the dictionary");
-            }
-
-            System.out.println("Word to scramble (Enter N or n to quit):");
-            
-            wordOrQuit = console.next();
         }
     }
 }
